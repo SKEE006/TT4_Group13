12ef18f1ca829095d5bf4a34bf28ec60548423e2
SKEE006 <78146627+SKEE006@users.noreply.github.com>
2021-05-07 00:52:27 +0800

    Split into Nav component and Login page. Home and Register page initiated.

--- test/src/App.js
+++ test/src/App.js
@@ -1,42 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Login from "./pages/login.js"
+import Nav from "./nav/nav.js"
 
 function App() {
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Home</a>
-          <div>
-            <ul className="navbar-nav me-auto mb-2 mb-md-0">
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Login</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Register</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Nav/>
 
       <main className="form-signin">
-        <form>
-          <img src="https://www.pinclipart.com/picdir/big/117-1177303_1-dbs-group-holdings-ltd-dbs-bank-logo.png" width="250" height="auto"/>
-            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-
-          <div className="form-floating">
-            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-              <label for="floatingInput">Email address</label>
-          </div>
-
-          <div className="form-floating">
-            <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-              <label for="floatingPassword">Password</label>
-          </div>
-
-          <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-        </form>
+        <Login/>
       </main>
     </div>
   );
