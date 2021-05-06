354e1d58fc853f38fc920bd06aa1c4b639b62f17
SKEE006 <78146627+SKEE006@users.noreply.github.com>
2021-05-07 01:33:49 +0800

    Nav done for now

--- test/src/nav/nav.js
+++ test/src/nav/nav.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Nav = () => {
   return(
     <div>
       <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Home</a>
+          <Link to="/" className="navbar-brand">Home</Link>
           <div>
             <ul className="navbar-nav me-auto mb-2 mb-md-0">
               <li className="nav-item">
-                <a className="nav-link active" href="#">Login</a>
+                <Link to="/login" className="navbar-brand">Login</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" href="#">Register</a>
+                <Link to="/register" className="navbar-brand">Register</Link>
               </li>
             </ul>
           </div>
--- test/src/pages/login.js
+++ test/src/pages/login.js
@@ -5,7 +5,7 @@ const Login = () => {
     <div>
       <form>
         <img src="https://www.pinclipart.com/picdir/big/117-1177303_1-dbs-group-holdings-ltd-dbs-bank-logo.png" width="250" height="auto"/>
-          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+          <h1 className="h3 mb-3 fw-normal">Please login</h1>
 
         <div className="form-floating">
           <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
@@ -17,7 +17,7 @@ const Login = () => {
             <label for="floatingPassword">Password</label>
         </div>
 
-        <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        <button className="w-100 btn btn-lg btn-primary" type="submit">Login</button>
       </form>
     </div>
   );
