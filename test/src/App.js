e6620c3dbd1a7efa4f488dac658298c69007f128
SKEE006 <78146627+SKEE006@users.noreply.github.com>
2021-05-07 00:38:28 +0800

    Initial page draft 1

--- test/src/App.js
+++ test/src/App.js
@@ -4,20 +4,40 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Home</a>
+          <div>
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">Login</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="#">Register</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <main className="form-signin">
+        <form>
+          <img src="https://www.pinclipart.com/picdir/big/117-1177303_1-dbs-group-holdings-ltd-dbs-bank-logo.png" width="250" height="auto"/>
+            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+          <div className="form-floating">
+            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+              <label for="floatingInput">Email address</label>
+          </div>
+
+          <div className="form-floating">
+            <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+              <label for="floatingPassword">Password</label>
+          </div>
+
+          <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        </form>
+      </main>
     </div>
   );
 }
